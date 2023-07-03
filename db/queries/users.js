788fbd712ac621f281e492ebaa9147b694bcc5d5
Phat Luong <luongtanphat25@gmail.com>
2023-07-03 18:52:29 -0400

    fixed api for create user

--- db/queries/users.js
+++ db/queries/users.js
@@ -1,10 +1,29 @@
-const db = require('../connection');
+const db = require("../connection");
 
 const getUsers = () => {
-  return db.query('SELECT * FROM users;')
-    .then(data => {
-      return data.rows;
-    });
+  return db.query("SELECT * FROM users;").then((data) => {
+    return data.rows;
+  });
 };
 
-module.exports = { getUsers };
+const addUser = (user) => {
+  const queryString = `INSERT INTO users(name, email, password) VALUES ($1, $2, $3) RETURNING *;`;
+  const values = [user.name, user.email, user.password];
+  return db
+    .query(queryString, values)
+    .then((result) => {
+      return result.rows[0];
+    })
+    .catch((err) => console.log(err));
+};
+
+const getUserByEmail = (email) => {
+  return db
+    .query(`SELECT * FROM users WHERE email = $1`, [email])
+    .then((result) => {
+      return result.rows[0];
+    })
+    .catch((err) => console.log(err));
+};
+
+module.exports = { getUsers, addUser, getUserByEmail };
--- routes/users-api.js
+++ routes/users-api.js
@@ -5,45 +5,41 @@
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
-const express = require('express');
-const router  = express.Router();
-const db = require('../db/connection');
-const bcrypt = require('bcryptjs');
-const userQueries = require('../db/queries/users');
+const express = require("express");
+const router = express.Router();
+const usersDB = require("../db/queries/users");
+const bcrypt = require("bcryptjs");
+const userQueries = require("../db/queries/users");
 
-router.get('/', (req, res) => {
-  userQueries.getUsers()
-    .then(users => {
+router.get("/", (req, res) => {
+  userQueries
+    .getUsers()
+    .then((users) => {
       res.json({ users });
     })
-    .catch(err => {
-      res
-        .status(500)
-        .json({ error: err.message });
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
     });
 });
 
-
-router.post('/sigin' ,(req, res) => {
-  const { name, email, password } = req.body;
-  const hashedPasword = bcrypt.hashSync(password, 30);
-
-  const queryString = `SELECT email FROM users WHERE email = $1`;
-  const findEmailInDB = db.query(queryString, [email]).then(result => {
-    return result.rows;
-  }).catch(err => console.log(err.message));
-
-  if (findEmailInDB.length > 0) {
-    console.log('Email already registed.');
-    return;
-  }
-
-  const insertQuery = `INSERT INTO users(name, email, password) VALUES ($1, $2, $3); RETURNING *;`;
-
-  return db.query(insertQuery, [name, email, hashedPasword]).then(result => {
-    console.log(result);
-    return result.rows;
-  }).catch(err => console.log(err.message));
+router.post("/", (req, res) => {
+  const user = req.body;
+  user.password = bcrypt.hashSync(user.password, 12);
+  usersDB
+    .getUserByEmail(user.email)
+    .then((registerdEmail) => {
+      if (!registerdEmail) {
+        usersDB
+          .addUser(user)
+          .then((result) => {
+            res.send("User created.");
+          })
+          .catch((err) => console.log(err));
+      } else {
+        res.send("Email already registered.");
+      }
+    })
+    .catch((err) => console.log(err));
 });
 
 module.exports = router;
