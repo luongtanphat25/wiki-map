84cf4ab30c9a1580ac7b82de6d6c6bfe260a54ee
Phat Luong <luongtanphat25@gmail.com>
2023-07-06 14:12:23 -0400

    fix layout of points view

--- public/scripts/mapView.js
+++ public/scripts/mapView.js
@@ -1,9 +1,6 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-var */
 $(() => {
-
-
-
   const url = `/api${window.location.pathname}`;
   let htmlContent = ``;
   $.ajax({ url: url }).then((json) => {
@@ -16,17 +13,15 @@ $(() => {
         '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
     }).addTo(map);
 
-
     for (const p of points) {
       const marker = L.marker([p.long, p.lat]).addTo(map);
       marker.bindPopup(`<b>${p.title}</b>`);
+
       htmlContent += `
-      <div class="card text-bg-dark m-3 col">
-        <div class="card-header"><h4>${p.title}</h4></div>
-        <img class="img" src=${p.image} />
-        <div class="card-body">
-          <p class="card-text">${p.description}</p>
-        </div>
+      <div class="card text-bg-light m-3" style="width: 25rem;">
+        <img src=${p.image} class="card-img-top"/>
+        <h5 class="card-header">${p.title}</h5>
+        <p class="card-text p-3">${p.description}</p>
       </div>`;
     }
 
