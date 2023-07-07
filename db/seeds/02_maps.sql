825886f7c6275b669c9175a947838cb6e855361b
Phat Luong <44761311+luongtanphat25@users.noreply.github.com>
2023-07-07 11:56:42 -0400

    Revert "Merge branch 'master' into feature/fav-map"
    This reverts commit 99e0c3ad1a97d41cd869ef720023ec259d0be413, reversing
    changes made to a2aadb88a42ee18dd4d129e14308c29a32959386.

--- db/seeds/02_maps.sql
+++ db/seeds/02_maps.sql
@@ -1,13 +1,4 @@
-<<<<<<< HEAD
-DROP TABLE IF EXISTS maps CASCADE;
-CREATE TABLE maps (
-  id SERIAL PRIMARY KEY NOT NULL,
-  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  favorite BOOLEAN NOT NULL DEFAULT FALSE
-);
-=======
 INSERT INTO maps(user_id, name) VALUES (1, 'Coffee Shop');
-INSERT INTO maps(user_id, name) VALUES (2, 'Bookstores');
+INSERT INTO maps(user_id, name) VALUES (2, 'Book store');
 INSERT INTO maps(user_id, name) VALUES (3, 'Brewery');
-INSERT INTO maps(user_id, name) VALUES (4, 'Beaches');
->>>>>>> 20e1c76770a22b00214a66e06e8bc64807ba548e
+INSERT INTO maps(user_id, name) VALUES (4, 'Beach');
